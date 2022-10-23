e6da9420084fb4e1446d7f2aa04ad63c76cd5ed9
DESKTOP-OGTMA5V\Ran <yellowzerow@gmail.com>
2022-10-23 21:03:38 +0900

    project init

---
+++ src/test/java/zerobase/thinkingbinding/ThinkingBindingApplicationTests.java
@@ -0,0 +1,13 @@
+package zerobase.thinkingbinding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinkingBindingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
